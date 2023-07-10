1a2ae747d698383a7893feb43a6d96f507342b6a
Cameron Loken <camloken@gmail.com>
2023-07-10 16:25:08 -0600

    Update results header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,19 +2,21 @@ import React, { Fragment} from 'react';
 import Rings from '../img/olympic-rings.svg'
 
 const Header = props => {
-  const genderTitle = props.gender && props.gender.value === 'M' ? 'Mens ' : 'Womens '
+  // const genderTitle = props.gender && props.gender.value === 'M' ? 'Mens ' : 'Womens '
+
   return (
     <header className="app-header text-center">
-      <div className="flex-row flex-align-center">
+      <div className="app-header-inner">
           {props.results.length < 1
             ? <Fragment>
                 <img src={Rings} alt="logo" className="logo" />
                 <div className="pr-20">Olympic Medalists</div>
               </Fragment>
-            : <div onClick={props.onClick} className="cancelable-title">
-                <div className="close-x">&times;</div>
-                <div>{`${props.year.value} ${genderTitle}${props.sport.value}`}</div>
-              </div>}
+            : <>
+                <img src={Rings} alt="logo" className="logo small fade-in" />
+                <div className='flex-1 fade-in'>{`${props.year.value} Games`}</div>
+                <div onClick={props.onClick} className="close-x fade-in">&times;</div>
+              </>}
         </div>
     </header>
   )
